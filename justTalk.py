62d805932843eb3aac8dd13d3baa2bd4aff2d2ad
Robert <rdtheriault@yahoo.com>
2017-04-11 10:15:41 -0700

    Create justTalk.py

---
+++ justTalk.py
@@ -0,0 +1,17 @@
+from sense_hat import SenseHat
+import time
+
+s = SenseHat()
+s.low_light = True
+
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+msg = "Hello World"
+
+while True: 
+    s.show_message(msg,text_colour=yellow)
